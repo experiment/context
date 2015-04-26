3be9ab014e484e058db401135ebeca7b6fdd44ca
Ryan Lower <rpjlower@gmail.com>
2015-04-25 18:36:31 -0700

    Include front plugin js

--- config/environment.js
+++ config/environment.js
@@ -6,6 +6,9 @@ module.exports = function(environment) {
     environment: environment,
     baseURL: '/',
     locationType: 'auto',
+    contentSecurityPolicy: {
+      'script-src': "'self' 'unsafe-eval' https://dl.frontapp.com"
+    },
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
