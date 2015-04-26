763b392d4ab0569adebbebd3afd31b1586025439
Ryan Lower <rpjlower@gmail.com>
2015-04-25 18:39:49 -0700

    Include ngrok in allowed script sources in development For testing the plugin within front (which requres https)

--- config/environment.js
+++ config/environment.js
@@ -28,6 +28,10 @@ module.exports = function(environment) {
     // ENV.APP.LOG_TRANSITIONS = true;
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
     // ENV.APP.LOG_VIEW_LOOKUPS = true;
+
+    ENV.contentSecurityPolicy = {
+      'script-src': "'self' 'unsafe-eval' https://dl.frontapp.com *.ngrok.com *.ngrok.com:35729" // Add ngrok for testing
+    }
   }
 
   if (environment === 'test') {
